3d6f50bbe30e73e812e1ff1bf3933859a1785cd4
Breno Santos <brenosantos145@gmail.com>
2024-06-12 21:15:00 -0300

    feat: basic cli

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/breno5g/kmk-cli/config"
+	"github.com/urfave/cli/v2"
 )
+
+func kmkInit() {
+	app := &cli.App{
+		Name:  "kmk-cli",
+		Usage: "Command line interface to download manga from Komikku",
+		Action: func(*cli.Context) error {
+			fmt.Println("Hello, World!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	err := config.Init()
@@ -13,6 +31,8 @@ func main() {
 		logger.Error(fmt.Sprintf("error initializing config: %v", err))
 		return
 	}
+
+	kmkInit()
 
 	// var mangas entity.Manga
 	// res, err := mangas.GetAllMangas(config.GetDB(), logger)
--- go.mod
+++ go.mod
@@ -4,4 +4,13 @@ go 1.21.3
 
 require github.com/mattn/go-sqlite3 v1.14.22
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/urfave/cli/v2 v2.27.2
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+)
